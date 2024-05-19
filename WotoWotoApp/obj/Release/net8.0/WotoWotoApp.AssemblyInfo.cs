0ae454f40316c34a010e134618419c2960ce313d
SD Olaide <sdolaide28@gmail.com>
2024-05-19 16:45:56 +0400

    app published and zip file created

--- WotoWotoApp/obj/Release/net8.0/WotoWotoApp.AssemblyInfo.cs
+++ WotoWotoApp/obj/Release/net8.0/WotoWotoApp.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WotoWotoApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb5cf983a64b0e3bc5dc3a07d94e70a4cc51c378")]
 [assembly: System.Reflection.AssemblyProductAttribute("WotoWotoApp")]
